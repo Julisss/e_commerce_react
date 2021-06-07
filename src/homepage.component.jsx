ad2b2821fe235ce653933ee8a76062d5c0a80d71
Julisss <48208674+Julisss@users.noreply.github.com>
2021-06-07 14:46:55 +0300

    v1

---
+++ src/homepage.component.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import './homepage.styles.scss'
+
+const HomePage = () => {
+    return (
+        <div className="homepage">
+            <div className="container">
+                <ul className="category-list">
+                    <li className="category-item">
+                        <div className="content">
+                            <h2 className="title">HATS</h2>
+                            <p className="desciption">Some text description</p>
+                        </div>
+                    </li>
+                    <li className="category-item">
+                        <div className="content">
+                            <h2 className="title">HATS</h2>
+                            <p className="desciption">Some text description</p>
+                        </div>
+                    </li>
+                    <li className="category-item">
+                        <div className="content">
+                            <h2 className="title">HATS</h2>
+                            <p className="desciption">Some text description</p>
+                        </div>
+                    </li>
+                    <li className="category-item">
+                        <div className="content">
+                            <h2 className="title">HATS</h2>
+                            <p className="desciption">Some text description</p>
+                        </div>
+                    </li>
+                    <li className="category-item">
+                        <div className="content">
+                            <h2 className="title">HATS</h2>
+                            <p className="desciption">Some text description</p>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default HomePage;
